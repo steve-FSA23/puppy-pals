063b1bd84f53ff0cf4b31fad4e5225e078d92bc9
Steve De La Rosa <stevedelarosa1036@gmail.com>
2024-01-23 21:58:37 -0400

    added comments to code

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,18 @@ import { puppyList } from "./data.js";
 import "./App.css";
 
 const App = () => {
+    // State to manage the list of puppies
     const [puppies, setPuppies] = useState(puppyList);
+
+    // State to track the ID of the featured puppy
     const [featPupId, setFeatPupId] = useState(null);
+
+    // Finding the featured puppy based on the ID
     const featuredPup = puppies.find((pup) => pup.id === featPupId);
 
+    // Handling click events on puppy items
     function handleClick(puppyId) {
+        // Setting the ID of the clicked puppy as the featured puppy ID
         setFeatPupId(puppyId);
     }
 
